7792fc0ddb8297aa24a6aeb26d860fe8e388ca95
Jose Avalos <luizavalos40@gmail.com>
2024-07-15 14:55:35 -0500

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default async function Home() {
-  return <div className="">Home</div>;
+import React from "react";
+import Summarizer from "./summarizer/Summarizer";
+import { summarize } from "./classify/pipeline";
+
+export default function page() {
+  return <Summarizer summarize={summarize} />;
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-start">
+        <a className="navbar-item text-2xl">Summarizer</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
