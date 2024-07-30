e11d0d7691cf1920b5a48967e7cc4424b5fb1ce6
Jose Avalos <luizavalos40@gmail.com>
2024-07-30 17:01:51 -0500

    add a nice no bs message next to nav.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ function Navbar() {
     <div className="navbar">
       <div className="navbar-start">
         <a className="navbar-item text-2xl">Summarizer</a>
+        <p>Free4Ever NO BS!</p>
       </div>
     </div>
   );
